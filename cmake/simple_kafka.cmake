dc1d3f1ee5cf1cff4e17af59a029bba4d65e00d6
Joaquin Bejar <jb@taunais.com>
2026-09-19 09:58:33 +0200

    chore: release v0.1.6

--- cmake/simple_kafka.cmake
+++ cmake/simple_kafka.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(simple_kafka
         GIT_REPOSITORY https://github.com/joaquinbejar/simple_kafka.git
-        GIT_TAG v0.1.1
+        GIT_TAG v0.1.2
         )
 FetchContent_MakeAvailable(simple_kafka)
 
